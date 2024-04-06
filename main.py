ac9ce1ddda4114cae133cc22c3bbf1ca6a358439
loris <loris.filliger@gmail.com>
2024-04-06 22:17:00 +0200

    v0.1.0 - ajout des différents menus, sélection de la couleur du joueur 1

--- main.py
+++ main.py
@@ -1,22 +1,55 @@
 import pyxel
 
-class App:
+class Menu:
     def __init__(self):
-        pyxel.init(320, 180, title='test')
-        self.j1 = Joueur('téliau', 0, 5)
+        self.colors = [1,2,4,5,6,8,9,10,12,13,14,15]
+        self.col1_id = 3
+        self.col2_id = 5
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btn(pyxel.KEY_SPACE):
+            if self.col1_id == self.col2_id:
+                pass
+            else:
+                Jeu(self.colors[self.col1_id])
+
+        if pyxel.btnp(pyxel.KEY_RIGHT):
+            if self.col1_id == 11:
+                self.col1_id = 0
+            else: self.col1_id += 1
+        elif pyxel.btnp(pyxel.KEY_LEFT):
+            if self.col1_id == 0:
+                self.col1_id = 11
+            else: self.col1_id -= 1
+
+    def draw(self):
+        pyxel.mouse(True)
+        pyxel.cls(0)
+        pyxel.text(5,6,"test",7)
+        pyxel.circ(124,160,6,self.colors[self.col1_id])
+        pyxel.circ(180, 160, 6, self.colors[self.col2_id])
+
+class Jeu:
+    def __init__(self, col1, col2):
+        self.j1 = Joueur(0, self.col1)
         pyxel.run(self.update, self.draw)
 
     def update(self):
         self.j1.update()
 
     def draw(self):
-        pyxel.cls(0)
+        pyxel.cls(11)
         self.j1.draw()
 
 
 class Joueur:
-    def __init__(self, name, team, color):
-        self.name = name
+    def __init__(self, team, color):
+        """
+        :param team: équipe
+        :param color: couleur
+        """
         self.team = team
         self.x = 10
         self.y = 10
@@ -24,7 +57,6 @@ class Joueur:
         self.points = 0
         self.speed = 3
         self.color = color
-        self.prev_color = self.color
 
     def update(self):
         if pyxel.btn(pyxel.KEY_Z):
@@ -39,5 +71,18 @@ class Joueur:
     def draw(self):
         pyxel.circ(self.x, self.y, self.size, self.color)
 
+##############################################################
+
+class App:
+    """classe mère qui lance la fenêtre"""
+    def __init__(self):
+        pyxel.init(320, 180, title='test')
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        Menu()
 
 App()
