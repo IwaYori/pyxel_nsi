501031c28eca736f0738b6c7119380363dc584a8
loris <loris.filliger@gmail.com>
2024-04-07 11:14:55 +0200

    v0.1.1 - code commenté, son ne fonctionne toujours pas

--- main.py
+++ main.py
@@ -2,27 +2,52 @@ import pyxel
 
 class Menu:
     def __init__(self):
+        # initialisation de la liste des couleurs possibles, on excluera le vert qui est la couleur du terrain
         self.colors = [1,2,4,5,6,8,9,10,12,13,14,15]
+        # par défaut, joueur1 = colors[3] = 5 (bleu), joueur2 colors[5] = 8 (rouge)
         self.col1_id = 3
         self.col2_id = 5
 
         pyxel.run(self.update, self.draw)
 
     def update(self):
+        # espace est la touche qui permet de lancer le jeu
         if pyxel.btn(pyxel.KEY_SPACE):
+            # empêche de lancer le jeu si deux joueurs ont la même couleur et joue un son
             if self.col1_id == self.col2_id:
                 pyxel.play(0,4)
             else:
+                # lance le jeu avec comme paramètre les deux couleurs
                 Jeu(self.colors[self.col1_id],self.colors[self.col2_id])
 
+        # permet de changer la couleur du joueur 1 à l'aide:
+        # de 'Q' ou 'D', ou d'un clique gauche ou droit sur le joueur 1
         if pyxel.btnp(pyxel.KEY_D):
+            # pour éviter un dépassement d'indice:
             if self.col1_id == 11:
                 self.col1_id = 0
             else: self.col1_id += 1
         elif pyxel.btnp(pyxel.KEY_Q):
+            # pour éviter un dépassement d'indice:
             if self.col1_id == 0:
                 self.col1_id = 11
             else: self.col1_id -= 1
+        # vérification de la position de la souris sur le joueur 1
+        elif 118 <= pyxel.mouse_x <= 130 and 166 >= pyxel.mouse_y >= 154:
+            if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+                if self.col1_id == 0:
+                    self.col1_id = 11
+                else:
+                    self.col1_id -= 1
+            if pyxel.btnp(pyxel.MOUSE_BUTTON_RIGHT):
+                if self.col1_id == 11:
+                    self.col1_id = 0
+                else:
+                    self.col1_id += 1
+
+        ##############################################################
+        # permet de changer la couleur du joueur 2 à l'aide:
+        # de 'GAUCHE' ou 'DROITE', ou d'un clique gauche ou droit sur le joueur 2
 
         if pyxel.btnp(pyxel.KEY_RIGHT):
             if self.col2_id == 11:
@@ -32,13 +57,26 @@ class Menu:
             if self.col2_id == 0:
                 self.col2_id = 11
             else: self.col2_id -= 1
+        #vérification de la position de la souris sur le joueur 2
+        elif 174 <= pyxel.mouse_x <= 186 and 166 >= pyxel.mouse_y >= 154:
+            if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+                if self.col2_id == 0:
+                    self.col2_id = 11
+                else:
+                    self.col2_id -= 1
+            if pyxel.btnp(pyxel.MOUSE_BUTTON_RIGHT):
+                if self.col2_id == 11:
+                    self.col2_id = 0
+                else:
+                    self.col2_id += 1
 
     def draw(self):
         pyxel.mouse(True)
         pyxel.cls(0)
-        pyxel.text(5,6,"test",7)
+        pyxel.text(5,6,"HaxBoule",7)
         pyxel.circ(124,160,6,self.colors[self.col1_id])
         pyxel.circ(180, 160, 6, self.colors[self.col2_id])
+
 
 class Jeu:
     def __init__(self, col1, col2):
@@ -71,6 +109,7 @@ class Joueur1:
         self.color = color
 
     def update(self):
+        # permet le déplacement du joueur par rapport à la vitesse
         if pyxel.btn(pyxel.KEY_Z):
             self.y = self.y - self.speed
         elif pyxel.btn(pyxel.KEY_S):
@@ -99,6 +138,7 @@ class Joueur2:
         self.color = color
 
     def update(self):
+        # permet le déplacement du joueur par rapport à la vitesse
         if pyxel.btn(pyxel.KEY_UP):
             self.y = self.y - self.speed
         elif pyxel.btn(pyxel.KEY_DOWN):
@@ -111,7 +151,9 @@ class Joueur2:
     def draw(self):
         pyxel.circ(self.x, self.y, self.size, self.color)
 
+
 ##############################################################
+
 
 class App:
     """classe mère qui lance la fenêtre"""
@@ -123,6 +165,7 @@ class App:
         pass
 
     def draw(self):
+        # appel de la classe Menu pour que le jeu se lance premièrement sur la fenêtre d'accueil
         Menu()
 
 App()
