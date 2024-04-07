ff63baf9c6ef811a4f464e7d21e9ea357ca160e0
loris <loris.filliger@gmail.com>
2024-04-07 11:33:37 +0200

    v0.1.2 - nom des couleurs ajoutés, qq bugs fix - music à fix

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import pyxel
 
 class Menu:
     def __init__(self):
-        # initialisation de la liste des couleurs possibles, on excluera le vert qui est la couleur du terrain
-        self.colors = [1,2,4,5,6,8,9,10,12,13,14,15]
-        # par défaut, joueur1 = colors[3] = 5 (bleu), joueur2 colors[5] = 8 (rouge)
+        # initialisation de la liste des couleurs possibles
+        self.colors = [1,2,4,5,8,9,10,12,13,14,15]
+        # par défaut, joueur1 = colors[3] = 5 (bleu), joueur2 colors[4] = 8 (rouge)
         self.col1_id = 3
-        self.col2_id = 5
-
+        self.col2_id = 4
         pyxel.run(self.update, self.draw)
 
     def update(self):
+
         # espace est la touche qui permet de lancer le jeu
         if pyxel.btn(pyxel.KEY_SPACE):
             # empêche de lancer le jeu si deux joueurs ont la même couleur et joue un son
@@ -24,13 +24,13 @@ class Menu:
         # de 'Q' ou 'D', ou d'un clique gauche ou droit sur le joueur 1
         if pyxel.btnp(pyxel.KEY_D):
             # pour éviter un dépassement d'indice:
-            if self.col1_id == 11:
+            if self.col1_id == 10:
                 self.col1_id = 0
             else: self.col1_id += 1
         elif pyxel.btnp(pyxel.KEY_Q):
             # pour éviter un dépassement d'indice:
             if self.col1_id == 0:
-                self.col1_id = 11
+                self.col1_id = 10
             else: self.col1_id -= 1
         # vérification de la position de la souris sur le joueur 1
         elif 118 <= pyxel.mouse_x <= 130 and 166 >= pyxel.mouse_y >= 154:
@@ -50,12 +50,12 @@ class Menu:
         # de 'GAUCHE' ou 'DROITE', ou d'un clique gauche ou droit sur le joueur 2
 
         if pyxel.btnp(pyxel.KEY_RIGHT):
-            if self.col2_id == 11:
+            if self.col2_id == 10:
                 self.col2_id = 0
             else: self.col2_id += 1
         elif pyxel.btnp(pyxel.KEY_LEFT):
             if self.col2_id == 0:
-                self.col2_id = 11
+                self.col2_id = 10
             else: self.col2_id -= 1
         #vérification de la position de la souris sur le joueur 2
         elif 174 <= pyxel.mouse_x <= 186 and 166 >= pyxel.mouse_y >= 154:
@@ -80,8 +80,11 @@ class Menu:
 
 class Jeu:
     def __init__(self, col1, col2):
-        self.j1 = Joueur1(0, col1)
-        self.j2 = Joueur2(0, col2)
+        # associe à chaque couleur son nom
+        self.color_name = {1:'Bleu foncé',2:'Magenta',4:'Sang',5:'Bleu',8:'Rouge',9:'Orange',
+                           10:'Jaune',12:'Bleu ciel',13:'Gris',14:'Rose',15:'Beige'}
+        self.j1 = Joueur1(0, col1, self.color_name[col1])
+        self.j2 = Joueur2(0, col2, self.color_name[col2])
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -90,36 +93,38 @@ class Jeu:
 
         if pyxel.btnp(pyxel.KEY_P):
             pass # futur menu pause
+        #la combinaison de touche R + O fait revenir au menu directement
+        elif pyxel.btnp(pyxel.KEY_R):
+            if pyxel.btnp(pyxel.KEY_O):
+                Menu()
 
     def draw(self):
+        pyxel.mouse(False)
         pyxel.cls(11)
         self.j1.draw()
         self.j2.draw()
 
 
 class Joueur1:
-    def __init__(self, team, color):
-        """
-        :param team: équipe
-        :param color: couleur
-        """
+    def __init__(self, team, color, name):
         self.team = team
         self.x = 10
         self.y = 10
         self.size = 6
         self.points = 0
-        self.speed = 3
+        self.speed = 2
         self.color = color
+        self.name = name
 
     def update(self):
         # permet le déplacement du joueur par rapport à la vitesse
         if pyxel.btn(pyxel.KEY_Z):
             self.y = self.y - self.speed
-        elif pyxel.btn(pyxel.KEY_S):
+        if pyxel.btn(pyxel.KEY_S):
             self.y = self.y + self.speed
-        elif pyxel.btn(pyxel.KEY_Q):
+        if pyxel.btn(pyxel.KEY_Q):
             self.x = self.x - self.speed
-        elif pyxel.btn(pyxel.KEY_D):
+        if pyxel.btn(pyxel.KEY_D):
             self.x = self.x + self.speed
 
     def draw(self):
@@ -127,28 +132,25 @@ class Joueur1:
 
 
 class Joueur2:
-    def __init__(self, team, color):
-        """
-        :param team: équipe
-        :param color: couleur
-        """
+    def __init__(self, team, color, name):
         self.team = team
         self.x = 120
         self.y = 120
         self.size = 6
         self.points = 0
-        self.speed = 3
+        self.speed = 2
         self.color = color
+        self.name = name
 
     def update(self):
         # permet le déplacement du joueur par rapport à la vitesse
         if pyxel.btn(pyxel.KEY_UP):
             self.y = self.y - self.speed
-        elif pyxel.btn(pyxel.KEY_DOWN):
+        if pyxel.btn(pyxel.KEY_DOWN):
             self.y = self.y + self.speed
-        elif pyxel.btn(pyxel.KEY_LEFT):
+        if pyxel.btn(pyxel.KEY_LEFT):
             self.x = self.x - self.speed
-        elif pyxel.btn(pyxel.KEY_RIGHT):
+        if pyxel.btn(pyxel.KEY_RIGHT):
             self.x = self.x + self.speed
 
     def draw(self):
